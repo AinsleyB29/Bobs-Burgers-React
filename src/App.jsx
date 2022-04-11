dbe51328bc02c462da3e0cc91b64e6b45b94ce1f
Ainsley Brundage <abrundage95@gmail.com>
2022-04-11 16:46:00 -0400

    Recreated UI Project in React

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [width, setWidth] = useState('0%');
+  const [image, setImage] = useState(
+    'https://bobsburgers-api.herokuapp.com/images/characters/319.jpg'
+  );
+
+  function Title() {
+    return (
+      <div className="title">
+        <h1>Bob's Burger's</h1>
+      </div>
+    );
+  }
+
+  function Burger() {
+    const openNav = () => {
+      setWidth('20%');
+    };
+    return (
+      <div id="icon" onClick={openNav}>
+        🍔
+      </div>
+    );
+  }
+
+  function Character() {
+    return (
+      <div class="container">
+        <img id="mainImg" src={image} alt="character" />
+      </div>
+    );
+  }
+
+  function Menu() {
+    const closeNav = () => {
+      setWidth('0px');
+    };
+
+    function getCharacter(event) {
+      let bobCharacters;
+      fetch(
+        'https://bobsburgers-api.herokuapp.com/characters/[1,7,26,8,13,3,20,11,39,63,18,10,21,207]'
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          bobCharacters = data;
+
+          let character = bobCharacters.find(
+            (obj) => obj.name.replaceAll('"', '') === event.target.textContent
+          );
+          setImage(character.image);
+        });
+    }
+
+    return (
+      <div id="mySidenav" class="sidenav" style={{ width: width }}>
+        <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>
+          &times;
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Dottie Minerva
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Adam
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Alex Papasian
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Alfred
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Allen
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Allison
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Amy
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Angela
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Angie Moscatone
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Annie Cragston
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Asch
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          BeefGeek
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Captain Flarty
+        </a>
+        <a class="character" href="#" onClick={getCharacter}>
+          Ice Pick
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <Title />
+      <Burger />
+      <Character />
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
